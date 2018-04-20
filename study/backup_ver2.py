28dd5ac40202bb4b336a29c9b56511f825c465e4
chenxubiao <chenxubiaocxb@163.com>
2018-04-21 00:03:56 +0800

    modify:some test

--- study/backup_ver2.py
+++ study/backup_ver2.py
@@ -14,14 +14,24 @@ source = ['/var/log/mesh/pytest/a', '/var/log/mesh/pytest/b']
 target_dir = '/var/log/mesh/pytest/bak/'
 
 # 3. The files are backed up into a zip file.
-# 4. The name of the zip archive is the
-target = target_dir + time.strftime('%Y%m%d%H%M%S' + '.zip')
+# 4. The current day is the name of the sub directory in the main directory
+today = target_dir + time.strftime('%Y%m%d')
+# The current time is the name of the zip archive
+now = time.strftime('%H%M%S')
+# Create the sub directory if it isn't already there
+if not os.path.exists(today):
+    os.mkdir(today)
+    print 'Successful created directory', today
+
+# The name of the zip file
+target = today + os.sep + now + '.zip'
 
 # 5. We use the zip command (in Unix/Linux) to put the files in a zip archive
-zip_command = "zip -qr '%s'%s"%(target, ''.join(source))
+zip_command = "zip -qr '%s'%s" % (target, ''.join(source))
+tar = 'tar -zvcf %s%s -X /var/log/mesh/pytest/a/hcp.xls' % (target, ''.join(source))
 
 # Run the backup
-if os.system(zip_command) == 0:
+if os.system(tar) == 0:
     print 'Successful backup to', target
 else:
     print 'Backup failure'
